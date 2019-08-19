87419b7d2e129446eb0f62e8ad3726905072dd0c
Yagorka <gvozdetscky.egor@yandex.ru>
2019-08-19 14:47:36 +0400

    Небольшие изменения, добавление меню

--- src/main/java/org/gvozdetscky/Main.java
+++ src/main/java/org/gvozdetscky/Main.java
@@ -2,9 +2,17 @@ package org.gvozdetscky;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -12,17 +20,51 @@ public class Main extends Application {
 
         primaryStage.setTitle("Приложение по индитификации");
 
-        VBox root = new VBox();
+        BorderPane root = new BorderPane();
+
+        VBox vbox = new VBox();
 
         Button uploadImage1 = new Button("Загрузка изображения 1");
         Button uploadImage2 = new Button("Загрузка изображения 2");
 
         Scene scene = new Scene(root, 300, 300);
 
+        MenuBar menubar = new MenuBar();
+
+        Menu about = new Menu("Справка");
+
+        MenuItem menuAboutProg =new MenuItem("О программе");
+
+        menuAboutProg.setOnAction(event -> showAboutProgramm());
+
+        about.getItems().add(menuAboutProg);
+
+        menubar.getMenus().add(about);
+
         primaryStage.setScene(scene);
 
-        root.getChildren().addAll(uploadImage1, uploadImage2);
+        root.setTop(menubar);
+
+        root.setCenter(vbox);
+
+        vbox.getChildren().addAll(uploadImage1, uploadImage2);
 
         primaryStage.show();
+    }
+
+    private void showAboutProgramm() {
+        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+        dialog.initStyle(StageStyle.UTILITY);
+        dialog.setTitle("Инфо");
+        dialog.setHeaderText("Программа по распознаванию лиц!\n" +
+                             "Разработчик: Гвоздецкий Егор\n\n\n" +
+                             "Контакты: vk.com/gvozdetscky.egor");
+
+        Image image = new Image("https://sun9-26.userapi.com/c636224/v636224916/b5fa/Ah1tLNp24LQ.jpg");
+        ImageView imageView = new ImageView(image);
+
+        dialog.setGraphic(imageView);
+
+        dialog.showAndWait();
     }
 }
---
+++ src/main/java/org/gvozdetscky/constImage/ConstImage.java
@@ -0,0 +1,6 @@
+package org.gvozdetscky.constImage;
+
+public class ConstImage {
+
+    public static final String PATH_DEVELOPER_IMAGE = "C:\\Users\\Yagorka\\Pictures\\нейросеть\\result.png";
+}
